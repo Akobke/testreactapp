3b3ea347070f89657650dc6bfcafbf917d3a9ed4
Akobke <akobke@yahoo.com>
2022-06-01 19:47:21 -0400

    Added local storage for task saving
    Used local storage for task saving

--- src/Input.js
+++ src/Input.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { useState, useEffect, useRef } from 'react';
 
 function InputFeild(props){
-
-    const [inputt, setInput] = useState([]);
+    const existingTodos = localStorage.getItem('todos');
+    const [inputt, setInput] = useState(JSON.parse(existingTodos));
     const [text, setText] = useState('');
+
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(inputt));
+    },[inputt]);
     
     return(
         <>
             <input type="text" value={text} onChange={e => setText(e.target.value)}/>
             <input type='submit' value={props.text} onClick={() =>{
-                setText('');
                 setInput(arr => [...arr, text]);
-            }}/>     
+                setText('');
+            }}/>
+            <input type='button' value='clear' onClick={() =>{
+                setInput(inputt => []);
+            }}/>
         <div>
             <ol>
             {inputt.map(userInput => (<li key={userInput}>{userInput}</li>))}
