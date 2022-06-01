72fea380fd75b8b7183eb3a8492daa2a9e69c750
Adam Kobke <akobke@yahoo.com>
2022-06-01 13:41:25 -0400

    Fixed awful state management to be less awful

--- src/Input.js
+++ src/Input.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-function InputFeild(){
-    const getInputValue = (event)=>{
-        const userType = event.target.value;
-        console.log(userType);
-    };
+function InputFeild(props){
 
+    const [inputt, setInput] = useState([]);
+    const [text, setText] = useState('');
+    
     return(
-        <input type="text" onChange={getInputValue}/>
+        <>
+            <input type="text" value={text} onChange={e => setText(e.target.value)}/>
+            <input type='submit' value={props.text} onClick={() =>{
+                setText('');
+                setInput(arr => [...arr, text]);
+            }}/>     
+        <div>
+            <ol>
+            {inputt.map(userInput => (<li key={userInput}>{userInput}</li>))}
+            </ol>
+        </div>
+        </>
     ); 
 }
 
